a96335d48f7de31b26be5cbf19d71208130b65cc
Rishita Kotiyal <rs677988@dal.ca>
2025-08-14 19:55:10 -0300

    Switch Redis Caching from Java serialization to JSON serialization so the cached data is human readable

---
+++ src/main/java/com/cityhappenings/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.cityhappenings.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(30)) //Optional: cache expires after 30 min
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+    }
+}
