c57f7d21d924316a3e9f621c1dfdca474ca384c6
Rishita Kotiyal <rs677988@dal.ca>
2025-08-17 21:05:08 -0300

    Create Event Entity

---
+++ src/main/java/com/cityhappenings/model/Event.java
@@ -0,0 +1,21 @@
+package com.cityhappenings.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "events")
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private LocalDateTime localDateTime;
+    private String category;
+
+}
